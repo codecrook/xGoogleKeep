450b54a27b2a91436b1240a75a06ccb79babf623
Deeptiman Mallick <deeptiman_mallick@outlook.com>
2021-04-28 23:53:39 +0530

    ADD: function to create new notes and push them to notes array

--- App.js
+++ App.js
@@ -3,8 +3,11 @@
 
     class App {
         constructor() {
+            this.notes = [];
+
             this.$form = document.querySelector('#form');
             this.$noteTitle = document.querySelector('#note-title');
+            this.$noteText = document.querySelector('#note-text');
             this.$formButtons = document.querySelector('#form-buttons');
 
             this.addEventListeners();
@@ -14,6 +17,14 @@
             document.body.addEventListener('click', event => {
                 this.handleFormClick(event);
             });
+
+            this.$form.addEventListener('submit', event => {
+                event.preventDefault();
+
+                const title = this.$noteTitle.value;
+                const text = this.$noteText.value;
+                (title || text) && this.addNote({ title, text });
+            })
         }
 
         handleFormClick(event) {
@@ -31,6 +42,19 @@
             this.$form.classList.remove('form-open');
             this.$noteTitle.style.display = 'none';
             this.$formButtons.style.display = 'none';
+        }
+
+        addNote(note) {
+            const newNote = {
+                title: note.title,
+                text: note.text,
+                color: 'white',
+                id: this.notes.length > 0 ? this.notes[this.notes.length - 1].id + 1 : 1
+            };
+
+            this.notes = [...this.notes, newNote];
+
+            console.log(this.notes);
         }
     }
 
