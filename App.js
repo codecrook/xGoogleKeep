92da5d5085053599fc09b73d5bcfc3c8824a58c7
Deeptiman Mallick <deeptiman_mallick@outlook.com>
2021-04-29 21:51:19 +0530

    ADD: functionality to change note color

--- App.js
+++ App.js
@@ -19,6 +19,8 @@
             this.$modalText = document.querySelector(".modal-text");
             this.$modalCloseButton = document.querySelector('.modal-close-button');
 
+            this.$colorTooltip = document.querySelector('#color-tooltip');
+
             this.$placeholder = document.querySelector('#placeholder');
             this.$notes = document.querySelector('#notes');
 
@@ -30,6 +32,27 @@
                 this.handleFormClick(event);
                 this.selectNote(event);
                 this.openModal(event);
+            });
+
+            document.body.addEventListener('mouseover', event => {
+                this.openTooltip(event);
+            });
+
+            document.body.addEventListener('mouseout', event => {
+                this.closeTooltip(event);
+            });
+
+            this.$colorTooltip.addEventListener('mouseover', function () {
+                this.style.display = 'flex';
+            })
+
+            this.$colorTooltip.addEventListener('mouseout', function () {
+                this.style.display = 'none';
+            });
+
+            this.$colorTooltip.addEventListener('click', event => {
+                const { color } = event.target.dataset;
+                (color) && this.editNoteColor(color);
             });
 
             this.$form.addEventListener('submit', event => {
@@ -89,6 +112,21 @@
             this.$modal.classList.toggle('open-modal');
         }
 
+        openTooltip(event) {
+            if (!event.target.matches('.toolbar-color')) return;
+            this.id = event.target.dataset.id;
+            const noteCoords = event.target.getBoundingClientRect();
+            const horizontal = noteCoords.left;
+            const vertical = window.scrollY - 21;
+            this.$colorTooltip.style.transform = `translate(${horizontal}px, ${vertical}px)`;
+            this.$colorTooltip.style.display = 'flex';
+        }
+
+        closeTooltip(event) {
+            if (!event.target.matches('.toolbar-color')) return;
+            this.$colorTooltip.style.display = 'none';
+        }
+
         addNote({ title, text }) {
             const newNote = {
                 title,
@@ -120,6 +158,13 @@
             this.displayNotes();
         }
 
+        editNoteColor(color) {
+            this.notes = this.notes.map(note =>
+                note.id === Number(this.id) ? { ...note, color } : note
+            );
+            this.displayNotes();
+        }
+
         displayNotes() {
             const hasNotes = this.notes.length > 0;
             this.$placeholder.style.display = hasNotes ? 'none' : 'flex';
@@ -136,7 +181,7 @@
                         <div class="toolbar">
                             <img class="toolbar-delete" src="./img/delete.svg">
                             <img class="toolbar-edit" src="./img/edit.svg">
-                            <img class="toolbar-color" src="./img/palette.svg">
+                            <img class="toolbar-color" data-id="${note.id}" src="./img/palette.svg">
                         </div>
                     </div>
                 </div>
